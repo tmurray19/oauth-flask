4fa9d17022fe23ce8205833a221347855f6d0ba3
tmurray19 <me@taidghmurray.ie>
2019-09-05 12:42:37 +0100

    Pushing current workspace

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,16 @@ from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 from oauth import OAuthSignIn
 from app import app, db
+import os, sys
 from app.models import User
 import tweepy
+from TwitterAPI import TwitterAPI
 
 
 @app.route('/')
 def index():
+    # Store the user id and the company id, create an account with those two pieces of info stored
+    # Then just update the user info as required
     return render_template('index.html')
 
 
@@ -32,19 +36,21 @@ def oauth_callback(provider):
     if not current_user.is_anonymous:
         return redirect(url_for('index'))
     oauth = OAuthSignIn.get_provider(provider)
-    social_id, username, email, access_token, access_token_secret = oauth.callback()
-    print(social_id)
-    print(username)
-    print(email)
-    print(access_token)
-    print(access_token_secret)    
+    social_id, username, email, access_token, access_token_secret = oauth.callback()  
     if social_id is None:
         flash('Authentication failed.')
         return redirect(url_for('index'))
     user = User.query.filter_by(social_id=social_id).first()
     if not user:
-        # define access tokens in user accounts, based on which website they're querying
-        user = User(social_id=social_id, nickname=username, email=email, twitter_access_token=access_token, twitter_access_token_secret = access_token_secret)
+        if provider is 'twitter':
+            # define access tokens in user accounts, based on which website they're querying
+            user = User(social_id=social_id, nickname=username, email=email, twitter_access_token=access_token, twitter_access_token_secret = access_token_secret)
+        elif provider is 'google':
+            pass
+        elif provider is 'facebook':
+            pass
+        elif provider is 'instagram':
+            pass
         db.session.add(user)
         db.session.commit()
     login_user(user, True)
@@ -53,28 +59,57 @@ def oauth_callback(provider):
 @login_required
 @app.route('/upload/twitter')
 def send_twitter():
-    print("social_id", current_user.social_id)
-    print("nickname", current_user.nickname)
-    print("email", current_user.email)
-    print("twitter_access_token", current_user.twitter_access_token)
-    print("twitter_access_token_secret", current_user.twitter_access_token_secret)
-    print("facebook_access_token", current_user.facebook_access_token)
-    print("facebook_access_token_secret", current_user.facebook_access_token_secret)
-    print("insta_access_token", current_user.insta_access_token)
-    print("insta_access_token_secret", current_user.insta_access_token_secret)
-    print("youtube_access_token", current_user.youtube_access_token)
-    print("youtube_access_token_secret", current_user.youtube_access_token_secret)
-
     creds = app.config['OAUTH_CREDENTIALS']['twitter']
-    auth = tweepy.OAuthHandler(creds['id'], creds['secret'])  
 
-    print(type(current_user.twitter_access_token))
-    print(type(current_user.twitter_access_token_secret))
-    auth.set_access_token(current_user.twitter_access_token, current_user.twitter_access_token_secret)
+    twitter = TwitterAPI(
+        creds['id'], 
+        creds['secret'], 
+        current_user.twitter_access_token, 
+        current_user.twitter_access_token_secret
+    )    
 
-    api = tweepy.API(auth)
-    api.update_status('Updating using OAuth authentication via Tweepy!')
-    return "Tweeted!"
+    # TODO: CHANGE THIS TO BE IMPLICIT
+    VIDEO_FILENAME = 'D:/test.mp4'
+
+    bytes_sent = 0
+    total_bytes = os.path.getsize(VIDEO_FILENAME)
+    file = open(VIDEO_FILENAME, 'rb')
+
+    def check_status(r):
+        if r.status_code < 200 or r.status_code > 299:
+            print(r.status_code)
+            print(r.text)
+            sys.exit(0)
+
+    # initialize media upload and get a media reference ID in the response
+    r = twitter.request('media/upload', {'command':'INIT', 'media_type':'video/mp4', 'total_bytes':total_bytes})
+    check_status(r)
+
+    media_id = r.json()['media_id']
+    segment_id = 0
+
+    # start chucked upload
+    while bytes_sent < total_bytes:
+        chunk = file.read(4*1024*1024)
+    
+        # upload chunk of byets (5mb max)
+        r = twitter.request('media/upload', {'command':'APPEND', 'media_id':media_id, 'segment_index':segment_id}, {'media':chunk})
+        check_status(r)
+        segment_id = segment_id + 1
+        bytes_sent = file.tell()
+        print('[' + str(total_bytes) + ']', str(bytes_sent))
+
+    # finalize the upload
+    r = twitter.request('media/upload', {'command':'FINALIZE', 'media_id':media_id})
+    check_status(r)
+
+    # post Tweet with media ID from previous request
+    # TODO: CHANGE STATUS
+    stat = 'Video uploaded from python script.'
+    r = twitter.request('statuses/update', {'status': stat, 'media_ids':media_id})
+    check_status(r)
+    # Change this to redirect to a success page with the same data
+    return "Video Successfully uploaded!"
 
 @login_required
 @app.route('/upload/facebook')
