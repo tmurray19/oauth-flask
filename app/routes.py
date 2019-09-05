6410019dc87f2c5b5238493c6f7fc6554bbb37b8
tmurray19 <me@taidghmurray.ie>
2019-09-05 23:52:51 +0100

    Need to implement email flow

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, flash
+from flask import Flask, redirect, url_for, render_template, flash, session, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
@@ -8,23 +8,40 @@ import os, sys
 from app.models import User
 import tweepy
 from TwitterAPI import TwitterAPI
+import random, time
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
 
 
 @app.route('/')
-@login_required
 def index():
     # Store the user id and the company id, create an account with those two pieces of info stored
     # Then just update the user info as required
+    if current_user.is_anonymous:
+        return redirect("https://squarev.dev.mobi")
     return render_template('index.html')
 
-@app.route('/connect/UID=<int:uid>/COID=<int:companyid>')
+@app.route('/connect/COID=<int:companyid>/UID=<int:uid>')
 def test(uid, companyid):
     # Store the user id and the company id, create an account with those two pieces of info stored
     # Then just update the user info as required
     if current_user.is_anonymous:
-        user = User(uid=uid, coid=companyid)
-        db.session.add(user)
-        login_user(user)
+        user = User.query.filter_by(uid=uid).first()
+        if not user:
+            user = User(uid=uid, coid=companyid)
+            db.session.add(user)
+            db.session.commit()
+        login_user(user, True)
+    print(current_user)
     return render_template('index.html')
 
 @app.route('/logout')
@@ -35,22 +52,67 @@ def logout():
 
 @app.route('/authorize/<provider>')
 def oauth_authorize(provider):
-    if not current_user.is_anonymous:
-        return redirect(url_for('index'))
-    oauth = OAuthSignIn.get_provider(provider)
-    return oauth.authorize()
+    if provider == 'facebook' or provider == "instagram":
+        print("We should be here if anything other than twitter is clicked.")
+        return render_template('error.html')
+    if provider == 'google':
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            app.config['CLIENT_SECRETS_FILE'], 
+            scopes=app.config['SCOPES']
+        )
+
+        flow.redirect_uri = url_for('oauth2callback', _external=True)
+
+        authorization_url, state = flow.authorization_url(
+            access_type='offline',
+            include_granted_scopes='true'
+        )
+
+        # Store the state so the callback can verify the auth server response.
+        session['state'] = state
+
+        return redirect(authorization_url)
+    else:
+        oauth = OAuthSignIn.get_provider(provider)
+        return oauth.authorize()
+
+# For youtube
+@app.route('/oauth2callback')
+def oauth2callback():
+    state = session['state']
+    
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        app.config['CLIENT_SECRETS_FILE'], 
+        scopes=app.config['SCOPES'], 
+        state=state
+    )
+    
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
+    
+    authorization_response = request.url
+    flow.fetch_token(authorization_response=authorization_response) 
+    credentials = flow.credentials
+
+    print(credentials)
+    print(credentials_to_dict(credentials))
+
+    # TODO: WRITE THIS INFORMATION TO THE USER ACCOUNT
+    current_user.youtube_credentials = credentials_to_dict(credentials)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 
 @app.route('/callback/<provider>')
 def oauth_callback(provider):
-    if not current_user.is_anonymous:
-        return redirect(url_for('index'))
     oauth = OAuthSignIn.get_provider(provider)
-    social_id, access_token, access_token_secret = oauth.callback()  
+    social_id, access_token, access_token_secret = oauth.callback()
+    print(social_id)  
     if social_id is None:
         flash('Authentication failed.')
         return redirect(url_for('index'))
-    if provider is 'twitter':
+    print(provider=='twitter')
+    if provider == 'twitter':
         current_user.twitter_access_token = access_token
         current_user.twitter_access_token_secret = access_token_secret
         db.session.commit()
@@ -59,7 +121,8 @@ def oauth_callback(provider):
 @login_required
 @app.route('/upload/twitter')
 def send_twitter():
-    error = False
+    twitter_upload_error=False
+    twitter_status = "Video Successfully uploaded!"
     problem = "No error specified"
     creds = app.config['OAUTH_CREDENTIALS']['twitter']
 
@@ -81,13 +144,10 @@ def send_twitter():
         if r.status_code < 200 or r.status_code > 299:
             print(r.status_code)
             print(r.text)
-            if r.status_code == 400:
-                print("Video Length is too long")
-                print(type(r.text))
-                problem = r.text
-                error = True
-                return
-            sys.exit(0)
+            problem = r.text
+            twitter_upload_error = True
+            print("Problem occured")        
+            twitter_status = "Video not uploaded successfully. Here's why: {}".format(problem)
 
     # initialize media upload and get a media reference ID in the response
     r = twitter.request('media/upload', {'command':'INIT', 'media_type':'video/mp4', 'total_bytes':total_bytes})
@@ -117,10 +177,7 @@ def send_twitter():
     r = twitter.request('statuses/update', {'status': stat, 'media_ids':media_id})
     check_status(r)
     # Change this to redirect to a success page with the same data
-    if error:
-        return "Video not uploaded successfully. Here's why: {}".format(problem)
-    else:
-        return "Video Successfully uploaded!"
+    return twitter_status
 
 @login_required
 @app.route('/upload/facebook')
@@ -136,16 +193,39 @@ def send_facebook():
 @login_required
 @app.route('/upload/youtube')
 def send_youtube():
-    creds = app.config['OAUTH_CREDENTIALS']['twitter']
-    auth = tweepy.OAuthHandler(creds['id'], creds['secret'])
-    auth.set_access_token(User.twitter_access_token, User.twitter_access_token_secret)
+    credentials = google.oauth2.credentials.Credentials(**current_user.youtube_credentials)
+    youtube = build(
+        "youtube", 
+        "v3",
+        credentials=credentials
+    )
 
-    api = tweepy.API(auth)
-    api.update_status('Updating using OAuth authentication via Tweepy!')
+    body=dict(
+        snippet=dict(
+            title="TEst",
+            description="options.description",
+            tags="tags",
+            categoryId="22"
+        ),
+        status=dict(
+            privacyStatus="public"
+        )
+    )
+
+    insert_request = youtube.videos().insert(
+      part=",".join(list(body.keys())),
+      body=body,
+      media_body=MediaFileUpload("N:/test.mp4", chunksize=-1, resumable=True)
+    )
+
+    resumable_upload(insert_request)
+    
+    return "Uploaded video successfully!"
 
 @login_required
 @app.route('/upload/instagram')
 def send_insta():
+
     return render_template('error.html')
     creds = app.config['OAUTH_CREDENTIALS']['twitter']
     auth = tweepy.OAuthHandler(creds['id'], creds['secret'])
@@ -153,3 +233,48 @@ def send_insta():
 
     api = tweepy.API(auth)
     api.update_status('Updating using OAuth authentication via Tweepy!')
+
+
+def credentials_to_dict(credentials):
+    return {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes
+    }
+
+
+def resumable_upload(insert_request):
+    response = None
+    error = None
+    retry = 0
+    while response is None:
+        try:
+            print("Uploading file...")
+            status, response = insert_request.next_chunk()
+            if 'id' in response:
+                print("Video id '%s' was successfully uploaded." % response['id'])
+            else:
+                exit("The upload failed with an unexpected response: %s" % response)
+        except HttpError as e:
+            if e.resp.status in app.config['RETRIABLE_STATUS_CODES']:
+                error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+            else:
+                raise
+        except app.config['RETRIABLE_EXCEPTIONS'] as e:
+            error = "A retriable error occurred: %s" % e
+        
+        if error is not None:
+            print(error)
+            retry += 1
+        if retry > app.config['MAX_RETRIES']:
+            exit("No longer attempting to retry.")
+        
+        max_sleep = 2 ** retry
+        sleep_seconds = random.random() * max_sleep
+        print("Sleeping %f seconds and then retrying..." % sleep_seconds)
+        time.sleep(sleep_seconds)
+
+
